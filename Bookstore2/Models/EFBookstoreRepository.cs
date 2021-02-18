61d5e5246be50a8c110b3d7c5d3b723e15fefeca
bryankekauoha <bkk1361@gmail.com>
2021-02-17 17:05:42 -0700

    initial commit

---
+++ Bookstore2/Models/EFBookstoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Bookstore2.Models;
+
+namespace Bookstore2.Models
+{
+    public class EFBookstoreRespository : IBookRepository
+    {
+        private BookDbContext _context;
+
+        //constructor
+        public EFBookstoreRespository(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
+
